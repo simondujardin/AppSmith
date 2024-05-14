12cc6221a675be4ce246f5eacf0eed29bbf1315b
Simon DUJARDIN <sdujardin@agaetis.fr>
2024-05-14 13:25:06 +0000

    add auth

--- pages/Tasks/jsobjects/utils/utils.js
+++ pages/Tasks/jsobjects/utils/utils.js
@@ -2,9 +2,22 @@ export default {
 	selectedTask: undefined,
 
 	showCompleted: true,
-	
+
 	listState: 'today',
-	
+
+	run : async () =>{
+		try{
+			const user = jsonwebtoken.verify(appsmith.store.token, 'secret')
+			return user;
+		}catch(error){
+			return showAlert('Session expired. Please re-login','warning').then(()=> appsmith.mode === 'EDIT' ? '': navigateTo("Authentication") )
+		}
+	},
+
+	init: async() =>{
+		return await this.run();
+	},
+
 	setListState: (listState) => {
 		this.listState = listState;
 	},
@@ -23,7 +36,7 @@ export default {
 		const todaysTasks = allTasks.filter((task) => task.created_at.startsWith(today));
 
 		const incompleteTasks = todaysTasks.filter(t => t.is_complete === false);
-		
+
 		// Sort the todaysTasks array by the 'created_at' date in descending order
 		incompleteTasks.sort((a, b) => b.created_at.localeCompare(a.created_at));
 		return incompleteTasks
@@ -32,9 +45,9 @@ export default {
 	getPendingTasks: async () => {
 		const allTasks = await getAllTasks.run();
 		const pendingTasks = allTasks.filter((task) => !task.is_complete);
-		
+
 		const incompletePendingTasks = pendingTasks.filter(t => t.is_complete === false);
-		
+
 		incompletePendingTasks.sort((a, b) => a.id - b.id);
 		return incompletePendingTasks
 	},
@@ -50,34 +63,34 @@ export default {
 		const allTasks = await getAllTasks.run();
 		const today = new Date().toISOString().slice(0, 10); // Extract YYYY-MM-DD part
 		const overdueTasks = allTasks.filter((task) => task.deadline > today);
-		
+
 		const incompleteOverdueTasks = overdueTasks.filter(t => t.is_complete === false);
-		
+
 		incompleteOverdueTasks.sort((a, b) => a.id - b.id);
 		return incompleteOverdueTasks;
 	},
-	
-  addTask: async () => {
+
+	addTask: async () => {
 		await createTask.run();
 		await this.getTodaysTasks();
 		await this.getPendingTasks();
 		await this.getCompletedTasks();
 		await this.getOverdueTasks();
-		
+
 		closeModal('mdl_editTask');
-		
+
 		showAlert('Task Created!', 'success');
 	},
-	
-	  updateTask: async () => {
+
+	updateTask: async () => {
 		await updateTask.run();
 		await this.getTodaysTasks();
 		await this.getPendingTasks();
 		await this.getCompletedTasks();
 		await this.getOverdueTasks();
-		
+
 		closeModal('mdl_editTask');
-		
+
 		showAlert('Task Updated!', 'success');
 	},
 
